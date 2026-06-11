f05584817e6276031508448a1027772aea6ff2f6
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-06-11 12:46:38 +0300

    update

--- database/books_db.py
+++ database/books_db.py
@@ -11,18 +11,20 @@ class BooksDB:
         self.password='library'
         create()
 
-    def create_book(data:dict):
+    def create_book(data:dict) -> int:
         conn = connect()
         cursor = conn.cursor()
         sql = "INSERT INTO books (title, author, genre, is_available, borrowed_by_member_id) VALUES (%s, %s, %s, %s, %s)"
         values = (data['title'], data['author'], data['genre'], True, None)
         cursor.execute(sql, values)
+        new_id = cursor.lastrowid
         conn.commit()
         cursor.close()
         conn.close()
+        return new_id
 
 
-    def get_all_books():
+    def get_all_books() -> list:
         conn = connect()
         cursor = conn.cursor(dictionary=True)
         cursor.execute("select * from books")
@@ -32,15 +34,27 @@ class BooksDB:
         conn.close()
         return all_books
 
-    def get_book_by_id(id:int):
+    def get_book_by_id(id:int) -> dict:
         conn = connect()
         cursor = conn.cursor(dictionary=True)
-        cursor.execute("select * from books where id = %s", (id))
+        cursor.execute("select * from books where id = %s", (id,))
         book = cursor.fetchone()
         conn.commit()
         cursor.close()
         conn.close()
         return book
+    
+    def update_book(id:int, data:dict) -> int:
+        conn = connect()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("UPDATE books SET is_available = %s, borrowed_by_member_id = %s WHERE id = %s", (data["is_available"], data["borrowed_by_member_id"], id))
+        conn.commit()
+        updated = cursor.rowcount
+        cursor.close()
+        conn.close()
+        return updated
+
+
 
 
 
